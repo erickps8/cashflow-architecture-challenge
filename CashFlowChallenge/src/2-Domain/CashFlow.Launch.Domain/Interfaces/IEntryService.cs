d723797b63b08b16036da86dc4027212af47911d
Erick Pinheiro <erickps8@hotmail.com>
2026-05-21 08:43:57 -0300

    feat: adiciona serviço de lançamento

---
+++ CashFlowChallenge/src/2-Domain/CashFlow.Launch.Domain/Interfaces/IEntryService.cs
@@ -0,0 +1,9 @@
+﻿using CashFlow.Launch.Domain.Entities;
+using CashFlow.Launch.Domain.Enums;
+
+namespace CashFlow.Launch.Domain.Interfaces.Services;
+
+public interface IEntryService
+{
+    Task<Entry?> CreateAsync(decimal amount, EntryType type, string description, DateTime occurredAt);
+}
