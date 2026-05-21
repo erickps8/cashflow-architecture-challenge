dfbca941b420f3f05e8ec062af5ae490502049a9
Erick Pinheiro <erickps8@hotmail.com>
2026-05-21 08:47:55 -0300

    feat: configura launch api

---
+++ CashFlowChallenge/src/3-Infra/3.1-Data/CashFlow.Launch.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using CashFlow.Launch.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CashFlow.Launch.Infrastructure.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        string connectionString)
+    {
+        services.AddDbContext<CashFlowDbContext>(options =>
+        {
+            options.UseSqlite(connectionString);
+        });
+
+        return services;
+    }
+}
