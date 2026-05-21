36a601f4b2d2584ff843c8d8b16117a0fe2fd35a
Erick Pinheiro <erickps8@hotmail.com>
2026-05-21 08:36:41 -0300

    feat: adiciona padrão de notificações

---
+++ CashFlowChallenge/src/2-Domain/CashFlow.Launch.Domain/Notifications/INotificator.cs
@@ -0,0 +1,10 @@
+﻿namespace CashFlow.Launch.Domain.Notifications;
+
+public interface INotificator
+{
+    bool HasNotification();
+
+    List<Notification> GetNotifications();
+
+    void Handle(Notification notification);
+}
