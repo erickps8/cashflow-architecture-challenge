d0efdcd59591ca3802ec47931801c58bfb05cf00
Erick Pinheiro <erickps8@hotmail.com>
2026-05-21 08:46:28 -0300

    feat: configura launch api

---
+++ CashFlowChallenge/src/1-Application/CashFlow.Launch.Api/Program.cs
@@ -0,0 +1,33 @@
+using Asp.Versioning;
+using CashFlow.Launch.Infrastructure.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddApiVersioning(options =>
+{
+    options.DefaultApiVersion = new ApiVersion(1, 0);
+
+    options.AssumeDefaultVersionWhenUnspecified = true;
+
+    options.ReportApiVersions = true;
+});
+
+builder.Services.AddLaunchInfrastructure("Data Source=cashflow.db");
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ CashFlowChallenge/src/3-Infra/3.1-Data/CashFlow.Launch.Infrastructure/Extensions/DependencyInjectionExtensions.cs
@@ -0,0 +1,29 @@
+﻿using CashFlow.Launch.Domain.Interfaces;
+using CashFlow.Launch.Domain.Interfaces.Services;
+using CashFlow.Launch.Domain.Notifications;
+using CashFlow.Launch.Domain.Services;
+using CashFlow.Launch.Infrastructure.Context;
+using CashFlow.Launch.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CashFlow.Launch.Infrastructure.Extensions;
+
+public static class DependencyInjectionExtensions
+{
+    public static IServiceCollection AddLaunchInfrastructure(
+        this IServiceCollection services,
+        string connectionString)
+    {
+        services.AddDbContext<CashFlowDbContext>(options =>
+        {
+            options.UseSqlite(connectionString);
+        });
+
+        services.AddScoped<IEntryService, EntryService>();
+        services.AddScoped<INotificator, Notificator>();
+        services.AddScoped<IEntryRepository, EntryRepository>();
+
+        return services;
+    }
+}
