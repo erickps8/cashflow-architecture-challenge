a6d6942b3ed2022b6bbd59f67b794e84d183a0e0
Erick Pinheiro <erickps8@hotmail.com>
2026-05-21 15:40:26 -0300

    feat: implementa arquitetura base do consolidation domain

---
+++ CashFlowChallenge/src/Consolidation/2-Domain/CashFlow.Consolidation.Domain/Entities/Base/Entity.cs
@@ -0,0 +1,11 @@
+﻿namespace CashFlow.Consolidation.Domain.Entities.Base;
+
+public abstract class Entity
+{
+    protected Entity()
+    {
+        Id = Guid.NewGuid();
+    }
+
+    public Guid Id { get; set; }
+}
---
+++ CashFlowChallenge/src/Consolidation/2-Domain/CashFlow.Consolidation.Domain/Interfaces/IDailyConsolidationService.cs
@@ -0,0 +1,9 @@
+﻿using CashFlow.Consolidation.Domain.Entities;
+
+namespace CashFlow.Consolidation.Domain.Interfaces;
+
+public interface IDailyConsolidationService
+{
+    Task<IEnumerable<DailyConsolidation>> GetAllAsync();
+    Task<DailyConsolidation> ReprocessAsync();
+}
