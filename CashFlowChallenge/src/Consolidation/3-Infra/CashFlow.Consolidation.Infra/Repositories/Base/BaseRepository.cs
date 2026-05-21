4f4ab18666d58a8cf773fdfc8c6dd0630f6310c0
Erick Pinheiro <erickps8@hotmail.com>
2026-05-21 15:40:57 -0300

    feat: implementa infraestrutura de persistencia do consolidation

---
+++ CashFlowChallenge/src/Consolidation/3-Infra/CashFlow.Consolidation.Infra/Repositories/Base/BaseRepository.cs
@@ -0,0 +1,38 @@
+﻿using CashFlow.Consolidation.Domain.Entities.Base;
+using CashFlow.Consolidation.Domain.Interfaces.Base;
+using CashFlow.Consolidation.Infra.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace CashFlow.Consolidation.Infra.Repositories.Base;
+
+public class BaseRepository<TEntity> : IBaseRepository<TEntity>
+    where TEntity : Entity
+{
+    protected readonly CashFlowConsolidationDbContext _context;
+    protected readonly DbSet<TEntity> _dbSet;
+
+    public BaseRepository(CashFlowConsolidationDbContext context)
+    {
+        _context = context;
+        _dbSet = context.Set<TEntity>();
+    }
+
+    public async Task AddAsync(TEntity entity)
+    {
+        await _dbSet.AddAsync(entity);
+    }
+    public async Task<TEntity?> GetByIdAsync(Guid id)
+    {
+        return await _dbSet.FirstOrDefaultAsync(x => x.Id == id);
+    }
+
+    public async Task<List<TEntity>> GetAllAsync()
+    {
+        return await _dbSet.AsNoTracking().ToListAsync();
+    }
+
+    public async Task SaveChangesAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+}
