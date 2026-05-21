e3cd6575f86feb50a29513cc918d32639559a59c
Erick Pinheiro <erickps8@hotmail.com>
2026-05-21 08:43:08 -0300

    feat: implementa repository pattern

---
+++ CashFlowChallenge/src/2-Domain/CashFlow.Launch.Domain/Interfaces/Base/IBaseRepository.cs
@@ -0,0 +1,12 @@
+﻿using CashFlow.Launch.Domain.Entities.Base;
+
+namespace CashFlow.Launch.Domain.Interfaces.Base;
+
+public interface IBaseRepository<TEntity> where TEntity : Entity
+{
+    Task AddAsync(TEntity entity);
+
+    Task<TEntity?> GetByIdAsync(Guid id);
+
+    Task SaveChangesAsync();
+}
---
+++ CashFlowChallenge/src/3-Infra/3.1-Data/CashFlow.Launch.Infrastructure/Repositories/EntryRepository.cs
@@ -0,0 +1,15 @@
+﻿using CashFlow.Launch.Domain.Entities;
+using CashFlow.Launch.Domain.Interfaces;
+using CashFlow.Launch.Infrastructure.Context;
+using CashFlow.Launch.Infrastructure.Repositories.Base;
+
+namespace CashFlow.Launch.Infrastructure.Repositories;
+
+public class EntryRepository
+    : BaseRepository<Entry>, IEntryRepository
+{
+    public EntryRepository(CashFlowDbContext context)
+        : base(context)
+    {
+    }
+}
