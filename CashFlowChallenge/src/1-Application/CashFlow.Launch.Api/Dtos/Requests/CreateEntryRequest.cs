a218ea28b861187b996705356cc377394686d2b1
Erick Pinheiro <erickps8@hotmail.com>
2026-05-21 08:48:11 -0300

    feat: implementa endpoint de criação de lançamentos

---
+++ CashFlowChallenge/src/1-Application/CashFlow.Launch.Api/Dtos/Requests/CreateEntryRequest.cs
@@ -0,0 +1,14 @@
+﻿using CashFlow.Launch.Domain.Enums;
+
+namespace CashFlow.Launch.Api.Dtos.Requests;
+
+public class CreateEntryRequest
+{
+    public decimal Amount { get; set; }
+
+    public EntryType Type { get; set; }
+
+    public string Description { get; set; } = string.Empty;
+
+    public DateTime OccurredAt { get; set; }
+}
