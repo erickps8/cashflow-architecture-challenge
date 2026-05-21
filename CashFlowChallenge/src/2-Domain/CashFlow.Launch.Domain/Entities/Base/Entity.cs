f016ea09557f24b9b09532b321ad427a80501617
Erick Pinheiro <erickps8@hotmail.com>
2026-05-21 08:36:11 -0300

    feat: adiciona domínio de lançamentos

---
+++ CashFlowChallenge/src/2-Domain/CashFlow.Launch.Domain/Entities/Base/Entity.cs
@@ -0,0 +1,11 @@
+﻿namespace CashFlow.Launch.Domain.Entities.Base;
+
+public abstract class Entity
+{
+    protected Entity()
+    {
+        Id = Guid.NewGuid();
+    }
+
+    public Guid Id { get; set; }
+}
---
+++ CashFlowChallenge/src/2-Domain/CashFlow.Launch.Domain/Enums/EntryType.cs
@@ -0,0 +1,7 @@
+﻿namespace CashFlow.Launch.Domain.Enums;
+
+public enum EntryType
+{
+    Credit = 1,
+    Debit = 2
+}
