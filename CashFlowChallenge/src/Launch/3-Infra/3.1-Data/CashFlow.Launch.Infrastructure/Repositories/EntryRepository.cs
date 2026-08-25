b728894c36f04ee294129b1c48d50a729acd1cf6
Erick Pinheiro <erickps8@hotmail.com>
2026-08-25 14:33:40 -0300

    feat: implement monthly entry query

--- CashFlowChallenge/src/Launch/3-Infra/3.1-Data/CashFlow.Launch.Infrastructure/Repositories/EntryRepository.cs
+++ CashFlowChallenge/src/Launch/3-Infra/3.1-Data/CashFlow.Launch.Infrastructure/Repositories/EntryRepository.cs
@@ -2,14 +2,27 @@
 using CashFlow.Launch.Domain.Interfaces;
 using CashFlow.Launch.Infrastructure.Context;
 using CashFlow.Launch.Infrastructure.Repositories.Base;
+using Microsoft.EntityFrameworkCore;
 
 namespace CashFlow.Launch.Infrastructure.Repositories;
 
-public class EntryRepository
-    : BaseRepository<Entry>, IEntryRepository
+public class EntryRepository : BaseRepository<Entry>, IEntryRepository
 {
-    public EntryRepository(CashFlowDbContext context)
-        : base(context)
+    private readonly CashFlowDbContext _context;
+
+    public EntryRepository(CashFlowDbContext context) : base(context)
     {
+        _context = context;
+    }
+
+    public Task<List<Entry>> GetByPeriodAsync(DateTime start, DateTime end)
+    {
+        return _context.Entries
+            .AsNoTracking()
+            .Include(x => x.Account)
+            .Include(x => x.Category)
+            .Where(x => x.OccurredAt >= start && x.OccurredAt < end)
+            .OrderBy(x => x.OccurredAt)
+            .ToListAsync();
     }
 }
