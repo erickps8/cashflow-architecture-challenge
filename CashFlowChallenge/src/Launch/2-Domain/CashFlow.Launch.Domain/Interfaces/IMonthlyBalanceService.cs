6838435b8ebfde49267e21998171169d7bdba9ee
Erick Pinheiro <erickps8@hotmail.com>
2026-08-25 16:53:44 -0300

    feat: add monthly balance service contract

---
+++ CashFlowChallenge/src/Launch/2-Domain/CashFlow.Launch.Domain/Interfaces/IMonthlyBalanceService.cs
@@ -0,0 +1,9 @@
+using CashFlow.Launch.Domain.Models;
+
+namespace CashFlow.Launch.Domain.Interfaces.Services;
+
+public interface IMonthlyBalanceService
+{
+    Task<MonthlyBalanceSummary> GetMonthAsync(int year, int month, decimal openingBalance = 0);
+    Task<BalanceProjectionSummary> GetProjectionAsync(int startYear, int startMonth, int months, decimal initialBalance = 0);
+}
