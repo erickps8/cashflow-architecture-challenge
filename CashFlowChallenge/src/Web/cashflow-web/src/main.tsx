c8a8e1a1bbc06d87c4ed0019743e9e8d25be95fb
Erick Pinheiro <erickps8@hotmail.com>
2026-08-26 10:59:39 -0300

    style(web): load monthly balance styles

--- CashFlowChallenge/src/Web/cashflow-web/src/main.tsx
+++ CashFlowChallenge/src/Web/cashflow-web/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles.css'
 import './filter-overrides.css'
+import './monthly.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode><App /></React.StrictMode>
