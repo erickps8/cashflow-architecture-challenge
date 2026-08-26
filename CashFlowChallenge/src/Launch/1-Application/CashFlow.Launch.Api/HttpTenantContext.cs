f1538ae317f7bcdc4f4434546566dd03dd918171
Erick Pinheiro <erickps8@hotmail.com>
2026-08-26 18:46:13 -0300

    fix(multitenancy): identify HTTP tenant as non-system context

--- CashFlowChallenge/src/Launch/1-Application/CashFlow.Launch.Api/HttpTenantContext.cs
+++ CashFlowChallenge/src/Launch/1-Application/CashFlow.Launch.Api/HttpTenantContext.cs
@@ -1,9 +1,17 @@
 using CashFlow.Launch.Domain.Interfaces;
+
 namespace CashFlow.Launch.Api;
+
 public sealed class HttpTenantContext : ITenantContext
 {
     private readonly IHttpContextAccessor _http;
-    public HttpTenantContext(IHttpContextAccessor http){_http=http;}
-    public Guid GroupId => Guid.TryParse(_http.HttpContext?.User.FindFirst("group_id")?.Value,out var id)?id:Guid.Empty;
-    public bool HasGroup => GroupId!=Guid.Empty;
+
+    public HttpTenantContext(IHttpContextAccessor http) => _http = http;
+
+    public Guid GroupId => Guid.TryParse(
+        _http.HttpContext?.User.FindFirst("group_id")?.Value,
+        out var id) ? id : Guid.Empty;
+
+    public bool HasGroup => GroupId != Guid.Empty;
+    public bool IsSystem => false;
 }
