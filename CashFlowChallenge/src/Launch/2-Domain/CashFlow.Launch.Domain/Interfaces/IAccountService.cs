1b94d7949b6a58581538e04e5fbd78fc266db2ac
Erick Pinheiro <erickps8@hotmail.com>
2026-08-26 15:44:26 -0300

    feat(accounts): add update and deactivate operations

--- CashFlowChallenge/src/Launch/2-Domain/CashFlow.Launch.Domain/Interfaces/IAccountService.cs
+++ CashFlowChallenge/src/Launch/2-Domain/CashFlow.Launch.Domain/Interfaces/IAccountService.cs
@@ -6,6 +6,8 @@ namespace CashFlow.Launch.Domain.Interfaces.Services;
 public interface IAccountService
 {
     Task<Account> CreateAsync(string name, AccountType type, decimal initialBalance);
+    Task<Account?> UpdateAsync(Guid id, string name, AccountType type, decimal initialBalance);
+    Task<bool> DeleteAsync(Guid id);
     Task<List<Account>> GetAllAsync();
     Task<Account?> GetByIdAsync(Guid id);
 }
