a1278e4b22131024f2900d8933bc3d44b8567612
Erick Pinheiro <erickps8@hotmail.com>
2026-08-26 18:32:40 -0300

    fix(tenant): implement HTTP tenant context

--- CashFlowChallenge/src/Launch/1-Application/CashFlow.Launch.Api/HttpTenantContext.cs
+++ CashFlowChallenge/src/Launch/1-Application/CashFlow.Launch.Api/HttpTenantContext.cs
@@ -1,3 +1,9 @@
 using CashFlow.Launch.Domain.Interfaces;
 namespace CashFlow.Launch.Api;
-public sealed class HttpTenantContext:IITenantContext_PLACEHOLDER {}
+public sealed class HttpTenantContext : ITenantContext
+{
+    private readonly IHttpContextAccessor _http;
+    public HttpTenantContext(IHttpContextAccessor http){_http=http;}
+    public Guid GroupId => Guid.TryParse(_http.HttpContext?.User.FindFirst("group_id")?.Value,out var id)?id:Guid.Empty;
+    public bool HasGroup => GroupId!=Guid.Empty;
+}
