d757ccce8cbba0c642c501e4ba374a9ba874a931
Erick Pinheiro <erickps8@hotmail.com>
2026-08-28 20:59:15 -0300

    feat(android): configure CashFlow Capacitor app

---
+++ CashFlowChallenge/src/Web/cashflow-web/capacitor.config.ts
@@ -0,0 +1,13 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'cloud.plania.cashflow',
+  appName: 'CashFlow',
+  webDir: 'dist',
+  server: {
+    url: 'https://plania.cloud',
+    cleartext: false
+  }
+};
+
+export default config;
