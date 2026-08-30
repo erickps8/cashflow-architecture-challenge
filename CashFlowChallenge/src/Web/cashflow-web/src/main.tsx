619fd0c5c87083ec3c7ebb0ba5b37d8236ccf624
Erick Pinheiro <erickps8@hotmail.com>
2026-08-30 18:54:43 -0300

    style(web): standardize entrypoint formatting

--- CashFlowChallenge/src/Web/cashflow-web/src/main.tsx
+++ CashFlowChallenge/src/Web/cashflow-web/src/main.tsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import AppModern from './AppModern'
-import './styles.css'
-import './filter-overrides.css'
-import './monthly.css'
-import './entries.css'
-import './forms-modern.css'
-import './global-create.css'
-import './auth-mobile-fixes.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import AppModern from './AppModern';
+import './styles.css';
+import './filter-overrides.css';
+import './monthly.css';
+import './entries.css';
+import './forms-modern.css';
+import './global-create.css';
+import './auth-mobile-fixes.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode><AppModern /></React.StrictMode>
-)
+  <React.StrictMode>
+    <AppModern />
+  </React.StrictMode>,
+);
